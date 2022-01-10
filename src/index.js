d72fbf60846b51ce215e343127dd1ed28fd3e86c
guilhermeportella <guilhermeportella2@gmail.com>
2022-01-09 23:33:25 -0300

    Config BD

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const server = express();
 
 server.get('/teste', (req, res) => {
-    res.send('Backend ok! ');
+    res.send('Backend ok!! ');
 });
 
 server.listen(3000, () => {
     console.log('API ONLINE');
-
-
 });
